f644164bc8b75a2c3a53091ffba9af9b8715bb5c
dda-cunh <dmoutinh@student.42lisboa.com>
2023-04-05 22:10:39 +0100

    Subject to PDF

--- Get Subject/index.js
+++ Get Subject/index.js
@@ -1,10 +1,9 @@
-const fs = require('fs');
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
+const outputFilePath = '../' + process.argv[2] + '/' + process.argv[3] + '/subject.pdf';
 const url = 'https://leetcode.com/problems/' + process.argv[3];
 const elementClass = '_1l1MA';
-const outputFilePath = '../' + process.argv[2] + '/'
-            + process.argv[3] + '/subject.pdf';
 
 if (!url) {
   console.error('Usage: node index.js URL');
@@ -16,10 +15,24 @@ if (!url) {
   const page = await browser.newPage();
   await page.goto(url);
   await page.waitForSelector(`div.${elementClass}`);
+  const elementHandle = await page.$(`div.${elementClass}`);
+  const boundingBox = await elementHandle.boundingBox();
   await page.evaluate(() => {
     const links = document.querySelectorAll('a');
     links.forEach(link => link.removeAttribute('href'));
   });
-  await page.pdf({landscape: true, path: outputFilePath, format: 'A4', printBackground: true, displayHeaderFooter: false});
+  await page.pdf({
+    landscape: true,
+    path: outputFilePath,
+    format: 'A4',
+    printBackground: true,
+    displayHeaderFooter: false,
+    clip: {
+      x: boundingBox.x,
+      y: boundingBox.y,
+      width: boundingBox.width,
+      height: boundingBox.height
+    }
+  });
   await browser.close();
 })();
